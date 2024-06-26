caa5673d21ccb581a12eaf6e0565b01ec59cc1da
karthick77 <mail2bkarthick@gmail.com>
2024-06-26 19:14:31 +0530

    Running - in branchN

---
+++ src/test/java/ZData3/Running.java
@@ -0,0 +1,16 @@
+package ZData3;
+
+public class Running {
+
+	public static void main(String[] args) {
+		
+		String running = "Yes";
+		
+		if(running == "Yes") {
+			System.out.println("Continue Running");
+		}else {
+			System.out.println("Take Rest");
+		}
+	}
+
+}
