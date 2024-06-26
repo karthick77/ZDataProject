713780b4db304614fe8572f50044b506e11f8e79
karthick77 <mail2bkarthick@gmail.com>
2024-06-26 19:09:39 +0530

    Hungry code in branchM

---
+++ src/test/java/ZData1/Hungry.java
@@ -0,0 +1,16 @@
+package ZData1;
+
+public class Hungry {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes") {
+			System.out.println("Eat something...");
+		}else {
+			System.out.println("Else Work");
+		}
+	}
+
+}
